0691d054d0ba86060e5f245dd356e15db5e0281a
Frankie <frankie@gmail.com>
2024-10-25 14:44:57 +0700

    Add ton proof

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,18 @@
-import {TonConnectButton} from "@tonconnect/ui-react";
+import {TonConnect, TonConnectButton} from "@tonconnect/ui-react";
 import './header.scss';
 
 export const Header = () => {
+
+    const tonConnect = new TonConnect();
+
+    const getWallets =  async () => {
+        const wallets = await tonConnect.getWallets();
+        console.log(wallets)
+    }
+
     return <header>
         <span>My App with React UI</span>
         <TonConnectButton />
+        <button onClick={getWallets}>Test</button>
     </header>
 }
